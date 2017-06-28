e01b7a0b28f6a600d4b346e69f8b1a2507c8704b
mibac138 <mibac138@users.noreply.github.com>
2017-06-28 05:01:14 +0200

    Built-in support for Kotlin's nullability

--- binder/src/main/kotlin/com/github/mibac138/argparser/binder/KotlinNullabilitySyntaxGenerator.kt
+++ binder/src/main/kotlin/com/github/mibac138/argparser/binder/KotlinNullabilitySyntaxGenerator.kt
@@ -22,36 +22,15 @@
 
 package com.github.mibac138.argparser.binder
 
-import java.lang.reflect.Method
-import kotlin.reflect.KCallable
-import kotlin.reflect.full.declaredMemberFunctions
-import kotlin.reflect.full.extensionReceiverParameter
-import kotlin.reflect.full.instanceParameter
-import kotlin.reflect.jvm.kotlinFunction
+import com.github.mibac138.argparser.syntax.dsl.SyntaxElementDSL
+import kotlin.reflect.KParameter
 
 /**
- * Created by mibac138 on 10-05-2017.
+ * Created by mibac138 on 28-06-2017.
  */
-object MethodBinder {
-    val generator = SyntaxGeneratorManager(ArgSyntaxGenerator, KotlinNullabilitySyntaxGenerator)
-
-    @JvmStatic
-    fun bindMethod(owner: Any, name: String): BoundMethod? {
-        val func = owner::class.declaredMemberFunctions.firstOrNull { it.name == name } ?: return null
-
-        return bindMethod(func, owner)
-    }
-
-    @JvmStatic
-    fun bindMethod(method: KCallable<*>, owner: Any?): BoundMethod {
-        if ((method.instanceParameter != null || method.extensionReceiverParameter != null) && owner == null)
-            throw IllegalArgumentException("Method requires instance variable or extension receiver (owner) but it's null")
-
-        return CallableBoundMethod(method, owner, generator)
-    }
-
-    @JvmStatic
-    fun bindMethod(owner: Any, method: Method): BoundMethod {
-        return bindMethod(method.kotlinFunction!!, owner)
+object KotlinNullabilitySyntaxGenerator : SyntaxGenerator {
+    override fun generate(dsl: SyntaxElementDSL<*>, param: KParameter) {
+        if (param.isOptional)
+            dsl.required = false
     }
 }
