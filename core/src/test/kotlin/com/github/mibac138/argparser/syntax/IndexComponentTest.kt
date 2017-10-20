5b0ceaa4c2f810a3c440a007e46854d21556cbe7
mibac138 <mibac138@users.noreply.github.com>
2017-10-21 00:10:22 +0200

    Added auto assign index option to IndexComponent

---
+++ core/src/test/kotlin/com/github/mibac138/argparser/syntax/IndexComponentTest.kt
@@ -0,0 +1,37 @@
+package com.github.mibac138.argparser.syntax
+
+import com.github.mibac138.argparser.syntax.dsl.element
+import com.github.mibac138.argparser.syntax.dsl.syntaxContainer
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class IndexComponentTest {
+    @Test
+    fun autoAssign1() {
+        val syntax = syntaxContainer {
+            element(Any::class.java) { index = 3 }
+            element(Any::class.java) { index }
+        }
+
+        assertEquals(3, syntax.content[0].index)
+        assertEquals(4, syntax.content[1].index)
+    }
+
+    @Test
+    fun autoAssign2() {
+        val syntax = syntaxContainer {
+            element(Any::class.java) { index }
+            element(Any::class.java) { index }
+        }
+
+        assertEquals(0, syntax.content[0].index)
+        assertEquals(1, syntax.content[1].index)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun negativeIndex() {
+        syntaxContainer {
+            element(Any::class.java) { index = -2 }
+        }
+    }
+}
