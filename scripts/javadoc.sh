0dc3658dc60b891ca8098cf88f3c9156b56905e2
mibac138 <mibac138@users.noreply.github.com>
2017-10-22 00:20:58 +0200

    Fix Javadoc script

--- scripts/javadoc.sh
+++ scripts/javadoc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -v
 
-if [ "$TRAVIS_REPO_SLUG" == "mibac138/ArgParser" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [[ "$TRAVIS_BRANCH" == "master" || "$TRAVIS_TAG" != "" ]]; then
+if [ "$TRAVIS_REPO_SLUG" == "mibac138/ArgParser" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_TAG" == "" ]; then
 
   echo -e "Generating Javadoc\n"
 
@@ -19,21 +19,12 @@ if [ "$TRAVIS_REPO_SLUG" == "mibac138/ArgParser" ] && [ "$TRAVIS_PULL_REQUEST" =
 
 
   cd gh-pages
-  if [ "$TRAVIS_TAG" == "" ]; then
-    git rm -rf ./docs/nightly
-    mkdir -p "./docs/nightly" && cp -Rf $HOME/javadoc-latest/. "$_"
-  else
-    mkdir -p "./docs/$TRAVIS_TAG" && cp -Rf $HOME/javadoc-latest/. "$_"
-    mkdir -p "./docs/stable" && cp -Rf $HOME/javadoc-latest/. "$_"
-  fi
+  git rm -rf ./docs/nightly
+  mkdir -p "./docs/nightly" && cp -Rf $HOME/javadoc-latest/. "$_"
 
 
   git add -f .
-  if [ "$TRAVIS_TAG" == "" ]; then
-    git commit -m ":sparkles: Auto pushed commit $TRAVIS_COMMIT"
-  else
-    git commit -m ":label: Auto pushed tag $TRAVIS_TAG (commit $TRAVIS_COMMIT)"
-  fi
+  git commit -m ":sparkles: Auto pushed commit $TRAVIS_COMMIT"
   git push -fq origin gh-pages > /dev/null
   echo -e "Published Javadoc to gh-pages.\n"
 fi
